1383578d4b223a7d7fba275160e7bd478484a331
Taha <tahasecond@gmail.com>
2025-02-02 12:35:04 -0500

    Update index.js

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logoImage from '../../assets/images/buzz.svg.png';
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 function NavBar() {
   return (
     <nav className="navbar">
       {/* Clickable logo button that redirects to the homepage */}
-      <a href="/" className="navbar-home-button">
+      <Link to="/" className="navbar-home-button">
         <img src={logoImage} alt="Home Logo" className="home-logo" />
         <span className="home-text">GT Movies</span>
-      </a>
+      </Link>
 
-      <ul className="navbar-links">
-        <li><a href="/movies">Movies</a></li>
-        <li><a href="/reviews">My Reviews</a></li>
-        <li><a href="/cart">Cart</a></li>
-      </ul>
+        <ul className="navbar-links">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/movies">Movies</Link></li>
+            <li><Link to="/reviews">My Reviews</Link></li>
+        </ul>
 
-      <div className="navbar-auth">
-        <button>Sign In</button>
-        <button>Register</button>
-      </div>
+        <div className="navbar-auth">
+            <button>Sign In</button>
+            <button>Register</button>
+        </div>
     </nav>
   );
 }
