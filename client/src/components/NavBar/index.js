a8d841178afb0d36b23d7e7afd11f4938dac5122
Taha <tahasecond@gmail.com>
2025-02-02 12:28:12 -0500

    Got rid of anchor tags and implemented react router for navbar and searchbar

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,35 +1,26 @@
 import React from 'react';
 import logoImage from '../../assets/images/buzz.svg.png';
-import { Link } from 'react-router-dom';
 import './styles.css';
 
 function NavBar() {
   return (
     <nav className="navbar">
       {/* Clickable logo button that redirects to the homepage */}
-      <Link to="/" className="navbar-home-button">
+      <a href="/" className="navbar-home-button">
         <img src={logoImage} alt="Home Logo" className="home-logo" />
         <span className="home-text">GT Movies</span>
-      </Link>
+      </a>
 
-        <ul className="navbar-links">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/movies">Movies</Link></li>
-            <li><Link to="/reviews">My Reviews</Link></li>
-            <li><Link to="/about">About Us</Link></li>
-        </ul>
+      <ul className="navbar-links">
+        <li><a href="/movies">Movies</a></li>
+        <li><a href="/reviews">My Reviews</a></li>
+        <li><a href="/cart">Cart</a></li>
+      </ul>
 
-<<<<<<< Updated upstream
       <div className="navbar-auth">
         <button>Sign In</button>
         <button>Register</button>
       </div>
-=======
-        <div className="navbar-auth">
-            <button>Sign In</button>
-            <button>Register</button>
-        </div>
->>>>>>> Stashed changes
     </nav>
   );
 }
