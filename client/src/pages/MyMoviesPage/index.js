2a8a0bcbfcf0e0a31a1d4b9710f0ce19b27f2e08
Dagim Tibebeselassie <164085795+DagimTibebeselassie@users.noreply.github.com>
2025-02-13 23:51:47 -0500

    Debugged MyMovies

--- client/src/pages/MyMoviesPage/index.js
+++ client/src/pages/MyMoviesPage/index.js
@@ -5,25 +5,22 @@ const MyMoviesPage = () => {
     const movies = new Array(14).fill(""); // Simulating 14 movie slots
 
     return (
-        <div>
-            <NavBar />
-            <div className="container">
-                <div className="pageHeader">
-                    <h1>My Movies</h1>
-                </div>
-                <div className="moviesGrid">
-                    {movies.map((_, index) => (
-                        <div key={index} className="movieContainer">
-                            <div className="movie"></div>
-                            <div className="btnContainer">
-                                <button>Download</button>
-                            </div>
+        <div className="container" style={{flexDirection: "column", backgroundColor: "white"}}>
+            <div className="pageHeader">
+                <h1>My Movies</h1>
+            </div>
+            <div className="moviesGrid">
+                {movies.map((_, index) => (
+                    <div key={index} className="movieContainer">
+                        <div className="movie"></div>
+                        <div className="btnContainer">
+                            <button>Download</button>
                         </div>
-                    ))}
-                </div>
-                <div className="extendedFooter">
-                    <p>Engineered by Yellow Jacket Spirit</p>
-                </div>
+                    </div>
+                ))}
+            </div>
+            <div className="extendedFooter">
+                <p>Engineered by Yellow Jacket Spirit</p>
             </div>
         </div>
     );
